8779617d2e040478444780f8455a2875779420b0
nyae <reoppldsxc@proton.me>
2026-05-09 19:28:59 -0700

    Publish to PIP + basic clean up

---
+++ src/docln_api/__init__.py
@@ -0,0 +1,7 @@
+from . import woh
+
+__version__ = "1.0.0"
+__author__ = "nyae"
+
+print("Please notice this is a work in progress, and the API may break.")
+print("(almost) everything is untested, im sowwy!")
